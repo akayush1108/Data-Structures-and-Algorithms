c61269a1b60a50a966f3008198b0466316950d77
Ayush Anand <ayush.anand6102000@gmail.com>
2022-08-11 18:34:57 +0530

    Power of Three (Leetcode-326)

---
+++ Recursion-lll/powerOfThree.java
@@ -0,0 +1,35 @@
+/*
+Problem Statement:
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+
+Example 2:
+Input: n = 0
+Output: false
+
+Example 3:
+Input: n = 9
+Output: true
+
+*/
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==0){
+            return false;
+        }
+        if(n==1 || n==3){
+            return true;
+        }
+        
+        if(n%3 != 0){
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
+}
