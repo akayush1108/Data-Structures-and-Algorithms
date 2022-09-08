fe191f30ba62e56667ef150a33ddbc712a771aaa
Ayush Anand <ayush.anand6102000@gmail.com>
2022-09-08 22:15:24 +0530

    Sum of nodes in binary search tree

---
+++ Binary Trees-I/SumOfNodesInBinaryTree.java
@@ -0,0 +1,82 @@
+/*
+Problem Statement :
+For a given Binary Tree of integers, find and return the sum of all the nodes data.
+Input Format:
+The first and the only line of input will contain the nodes data, all separated by a single space. 
+Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The first and the only line of output prints the sum of all the nodes data present in the binary tree.
+*/
+
+
+import java.util.Scanner;
+
+public class SumOfNodesInBinaryTree {
+	
+	public static int getSum(BinaryTreeNode<Integer> root)
+	{
+		if(root == null)     //base case
+			return 0;
+		
+		int leftSum = getSum(root.left);
+		int rightSum = getSum(root.right);
+		int totalSum = root.data + leftSum + rightSum;
+		return totalSum;
+	}
+	
+	
+	public static BinaryTreeNode<Integer> improvedInputBinaryTree(boolean isRoot, int parentData, boolean isLeftChild)
+	{
+		if(isRoot)
+		{
+			System.out.println("Enter root data :");
+		}
+		else
+		{
+			if(isLeftChild)
+			{
+				System.out.println("Enter left child of " + parentData);
+			}
+			else
+			{
+				System.out.println("Enter right child of " + parentData);
+			}
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		int rootData = sc.nextInt();
+		if(rootData == -1)   //There is no binary tree.
+		{
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+		BinaryTreeNode<Integer> leftChild = improvedInputBinaryTree(false, root.data, true);
+		BinaryTreeNode<Integer> rightChild = improvedInputBinaryTree(false, root.data, false);
+		root.left = leftChild;
+		root.right = rightChild;
+		return root;
+	}
+
+
+	public static void main(String args[])
+	{
+		BinaryTreeNode<Integer> root = improvedInputBinaryTree(true, 0, true);
+		System.out.println(getSum(root));
+		
+	}
+}
+
+//Node of Binary Tree
+
+class BinaryTreeNode<T>
+{
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+	public BinaryTreeNode(T data)
+	{
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
