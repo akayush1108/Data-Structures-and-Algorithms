d0ad88f316fe56f5b89e34ec4ce2771e20134a20
Ayush Anand <ayush.anand6102000@gmail.com>
2022-09-20 23:42:19 +0530

    Lowest common ancestors of a BST

--- BST-ll/LowestCommonAncestorOfBST.java
+++ BST-ll/LowestCommonAncestorOfBST.java
@@ -1,133 +1,49 @@
 /*
-Problem Statement: Lca Of Bst
-You are given a partially written BST class.
-You are required to complete the body of lca function. "lca" function is expected to 
-find the lowest commong ancestor of d1 and d2.
+Problem Statement: Lowest Common Ancestor of a Binary Search Tree (LeetCode-235).
 
-Sample Input:
-21
-50 25 12 n n 37 30 n n n 75 62 60 n n 70 n n 87 n n
-12
-30
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest
+node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
-Sample Output:
-25
+ 
+Example 1:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Example 2:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+
+
+Constraints:
+    The number of nodes in the tree is in the range [2, 105].
+    -109 <= Node.val <= 109
+    All Node.val are unique.
+    p != q
+    p and q will exist in the BST.
+
+
  */
-import java.io.*;
-import java.util.*;
 
-public class LowestCommonAncestorOfBST {
-  public static class Node {
-    int data;
-    Node left;
-    Node right;
 
-    Node(int data, Node left, Node right) {
-      this.data = data;
-      this.left = left;
-      this.right = right;
-    }
-  }
-
-  public static class Pair {
-    Node node;
-    int state;
-
-    Pair(Node node, int state) {
-      this.node = node;
-      this.state = state;
-    }
-  }
-
-  public static Node construct(Integer[] arr) {
-    Node root = new Node(arr[0], null, null);
-    Pair rtp = new Pair(root, 1);
-
-    Stack<Pair> st = new Stack<>();
-    st.push(rtp);
-
-    int idx = 0;
-    while (st.size() > 0) {
-      Pair top = st.peek();
-      if (top.state == 1) {
-        idx++;
-        if (arr[idx] != null) {
-          top.node.left = new Node(arr[idx], null, null);
-          Pair lp = new Pair(top.node.left, 1);
-          st.push(lp);
-        } else {
-          top.node.left = null;
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
         }
-
-        top.state++;
-      } else if (top.state == 2) {
-        idx++;
-        if (arr[idx] != null) {
-          top.node.right = new Node(arr[idx], null, null);
-          Pair rp = new Pair(top.node.right, 1);
-          st.push(rp);
-        } else {
-          top.node.right = null;
+        else if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);
         }
-
-        top.state++;
-      } else {
-        st.pop();
-      }
+        // data found
+        else{
+            return root;
+        }
     }
-
-    return root;
-  }
-
-  public static void display(Node node) {
-    if (node == null) {
-      return;
-    }
-
-    String str = "";
-    str += node.left == null ? "." : node.left.data + "";
-    str += " <- " + node.data + " -> ";
-    str += node.right == null ? "." : node.right.data + "";
-    System.out.println(str);
-
-    display(node.left);
-    display(node.right);
-  }
-
-  public static int lca(Node node, int d1, int d2) {
-	  
-    // write your code here
-	  
-    if(d1 < node.data && d2 < node.data){
-        return lca(node.left,d1,d2);
-    }
-    else if(d1 > node.data && d2 > node.data){
-        return lca(node.right,d1,d2);
-    }
-    else{
-        return node.data;
-    }
-  }
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    Integer[] arr = new Integer[n];
-    String[] values = br.readLine().split(" ");
-    for (int i = 0; i < n; i++) {
-      if (values[i].equals("n") == false) {
-        arr[i] = Integer.parseInt(values[i]);
-      } else {
-        arr[i] = null;
-      }
-    }
-
-    int d1 = Integer.parseInt(br.readLine());
-    int d2 = Integer.parseInt(br.readLine());
-
-    Node root = construct(arr);
-    int lca = lca(root, d1, d2);
-    System.out.println(lca);
-  }
-
 }
